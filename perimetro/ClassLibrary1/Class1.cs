16a48e9599dafc741acee6f56771db30a91c724c
brayanvaliente <48190483+brayanvaliente@users.noreply.github.com>
2019-03-07 07:51:35 -0600

    Add files via upload

---
+++ perimetro/ClassLibrary1/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class areas
+    {
+        public double bases;
+        public double alturas;
+
+        public void setbases(double valor)
+        {
+            bases = valor;
+
+        }
+
+        public double getbases()
+        {
+            return bases;
+
+        }
+
+        public void setalturas(double valor)
+        {
+            alturas = valor;
+
+        }
+
+        public double getalturas()
+        {
+            return bases;
+
+        }
+
+        public void imprimir()
+        {
+            Console.WriteLine("el perimetro de un cuadrados es"+((bases*2)+(alturas*2)));
+        } 
+
+
+
+    }
+
+    
+}
---
+++ perimetro/ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            areas obj = new areas();
+
+            obj.setbases(4);
+            Console.WriteLine(obj.getbases());
+
+            obj.setalturas(4);
+            Console.WriteLine(obj.getalturas());
+            obj.imprimir();
+            Console.ReadKey();
+
+        }
+    }
+}
