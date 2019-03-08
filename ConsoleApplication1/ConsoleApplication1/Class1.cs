80c3d2522f992bd14528806858dcf8ee01aeb90f
brayanvaliente <48190483+brayanvaliente@users.noreply.github.com>
2019-03-07 19:54:25 -0600

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Class1
+    {
+
+        int val1=4, val2=5, suma;
+        public void imprimir(){
+            suma = val1 + val2;
+            Console.WriteLine("la suma es " + suma);
+        }
+
+        public void imprimir(int valor1){
+            suma = val1 + valor1;
+            Console.WriteLine("la suma es " + suma);
+        }
+
+        public void imprimir(int valor1, int valor2)
+        {
+            suma = valor1 + valor2;
+            Console.WriteLine("la suma es " + suma);
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obj = new Class1();
+            Class1 obj1 = new Class1();
+
+            obj.imprimir();
+            obj.imprimir(3);
+            obj.imprimir(2,5);
+
+            Console.ReadKey();
+        }
+    }
+}
