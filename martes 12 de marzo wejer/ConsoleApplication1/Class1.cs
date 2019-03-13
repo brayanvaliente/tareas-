49b7612c8694de3b2bf688097ca248cd01ab35f1
brayanvaliente <48190483+brayanvaliente@users.noreply.github.com>
2019-03-12 19:45:25 -0600

    Add files via upload

---
+++ martes 12 de marzo wejer/ConsoleApplication1/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Class1
+    {
+        private double monto;
+        private string nombre;
+
+       public Class1() {
+            this.monto = 0;
+            this.nombre = "";
+        }
+
+        public Class1(double var1)
+        {
+            monto = var1;
+        }
+        public void imp(){
+            Console.WriteLine(monto);
+        
+        }
+        
+
+    }
+}
---
+++ martes 12 de marzo wejer/ConsoleApplication1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            double montos;
+            Console.WriteLine("ingrese montos");
+            montos = double.Parse(Console.ReadLine());
+            Class1 obj = new Class1(montos);
+            obj.imp();
+           
+            Console.ReadKey();
+
+            
+        }
+
+    }
+}
