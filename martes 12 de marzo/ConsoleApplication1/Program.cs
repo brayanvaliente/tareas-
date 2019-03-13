6e77c1a5a5b1c4e53718d6ee001e3249fedf72a6
brayanvaliente <48190483+brayanvaliente@users.noreply.github.com>
2019-03-12 19:44:01 -0600

    Add files via upload

---
+++ martes 12 de marzo/ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            cliente obj = new cliente();
+                
+                obj.depositar(35);
+                obj.retirar("brayan", 10);
+                Console.ReadKey();
+
+
+               
+            
+        }
+    }
+}
---
+++ martes 12 de marzo/ConsoleApplication1/cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class cliente
+    {
+        string nombre;
+        double monto;
+
+        public cliente()
+        {
+            this.nombre = "";
+            this.monto = 0;
+        }
+        public void depositar(double a){
+            monto = a;
+        }
+        public void retirar(string nom, double b){
+            Console.WriteLine("ingrese su nombre : "+ nom + "\nsu retiro es: "+(monto-b));
+        }
+
+    }
+}
