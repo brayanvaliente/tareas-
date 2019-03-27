e53cad2d22d2935755c0a09e0a3f37a55f8ecb69
brayanvaliente2019 <48190483+brayanvaliente2019@users.noreply.github.com>
2019-03-27 12:56:43 -0600

    Add files via upload

---
+++ MARTES 26 MARZO 4 calculadora/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frmOperaciones
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.resta = new System.Windows.Forms.Button();
+            this.resultado = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.val2 = new System.Windows.Forms.TextBox();
+            this.val1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "valor1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "valor2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.resta);
+            this.groupBox1.Controls.Add(this.resultado);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.val2);
+            this.groupBox1.Controls.Add(this.val1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(24, 23);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(230, 197);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(14, 174);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "multiplicar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // resta
+            // 
+            this.resta.Location = new System.Drawing.Point(97, 141);
+            this.resta.Name = "resta";
+            this.resta.Size = new System.Drawing.Size(75, 23);
+            this.resta.TabIndex = 7;
+            this.resta.Text = "resta";
+            this.resta.UseVisualStyleBackColor = true;
+            this.resta.Click += new System.EventHandler(this.resta_Click);
+            // 
+            // resultado
+            // 
+            this.resultado.Location = new System.Drawing.Point(97, 114);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(100, 20);
+            this.resultado.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Resultado";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 140);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "suma";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // val2
+            // 
+            this.val2.Location = new System.Drawing.Point(97, 80);
+            this.val2.Name = "val2";
+            this.val2.Size = new System.Drawing.Size(100, 20);
+            this.val2.TabIndex = 3;
+            // 
+            // val1
+            // 
+            this.val1.Location = new System.Drawing.Point(97, 37);
+            this.val1.Name = "val1";
+            this.val1.Size = new System.Drawing.Size(100, 20);
+            this.val1.TabIndex = 2;
+            this.val1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(97, 174);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "dividir";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // frmOperaciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "frmOperaciones";
+            this.Text = "CALCULADORA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox val2;
+        private System.Windows.Forms.TextBox val1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox resultado;
+        private System.Windows.Forms.Button resta;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ MARTES 26 MARZO 4 calculadora/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary2;
+namespace WindowsFormsApplication1
+{
+    public partial class frmOperaciones : Form
+    {
+        operaciones ingreso;
+        restas objresta;
+        multiplcaciones objmult;
+        divisiones objdiv;
+        public frmOperaciones()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ingreso = new operaciones();
+            ingreso.Val1 = double.Parse(val1.Text);
+            ingreso.Val2 = double.Parse(val2.Text);
+            resultado.Text = ingreso.calcular().ToString();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+             
+        }
+
+        private void resta_Click(object sender, EventArgs e)
+        {
+            objresta = new restas();
+            objresta.R1 = double.Parse(val1.Text);
+            objresta.R2 = double.Parse(val2.Text);
+            resultado.Text = objresta.calcular().ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            objmult = new multiplcaciones();
+            objmult.M1 = double.Parse(val1.Text);
+            objmult.M2 = double.Parse(val2.Text);
+            resultado.Text = objmult.calcular().ToString();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            objdiv = new divisiones();
+            objdiv.D1 = double.Parse(val1.Text);
+            objdiv.D2 = double.Parse(val2.Text);
+            resultado.Text = objdiv.calcular().ToString();
+        }
+    }
+}
