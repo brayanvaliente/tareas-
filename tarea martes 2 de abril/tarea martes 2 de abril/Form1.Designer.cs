a2557cd2bb2227fc4a15ec9b3624a15cb24e7595
brayanvaliente2019 <48190483+brayanvaliente2019@users.noreply.github.com>
2019-03-31 21:40:10 -0600

    Add files via upload

---
+++ tarea martes 2 de abril/tarea martes 2 de abril/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace tarea_martes_2_de_abril
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.val1 = new System.Windows.Forms.TextBox();
+            this.val2 = new System.Windows.Forms.TextBox();
+            this.resultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(112, 43);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(51, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "suma";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.checkBox1.Click += new System.EventHandler(this.checkBox1_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(283, 43);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(49, 17);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "resta";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            this.checkBox2.Click += new System.EventHandler(this.checkBox2_Click);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(112, 82);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(87, 17);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "multiplcacion";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(283, 82);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(61, 17);
+            this.checkBox4.TabIndex = 3;
+            this.checkBox4.Text = "division";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.CheckedChanged += new System.EventHandler(this.checkBox4_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(153, 231);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 41);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // val1
+            // 
+            this.val1.Location = new System.Drawing.Point(112, 130);
+            this.val1.Name = "val1";
+            this.val1.Size = new System.Drawing.Size(100, 20);
+            this.val1.TabIndex = 5;
+            // 
+            // val2
+            // 
+            this.val2.Location = new System.Drawing.Point(283, 129);
+            this.val2.Name = "val2";
+            this.val2.Size = new System.Drawing.Size(100, 20);
+            this.val2.TabIndex = 6;
+            // 
+            // resultado
+            // 
+            this.resultado.Location = new System.Drawing.Point(195, 178);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(100, 20);
+            this.resultado.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::tarea_martes_2_de_abril.Properties.Resources._102024371_mat_tablero;
+            this.ClientSize = new System.Drawing.Size(463, 314);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.val2);
+            this.Controls.Add(this.val1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox val1;
+        private System.Windows.Forms.TextBox val2;
+        private System.Windows.Forms.TextBox resultado;
+    }
+}
+
---
+++ tarea martes 2 de abril/tarea martes 2 de abril/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary2;
+namespace tarea_martes_2_de_abril
+{
+    public partial class Form1 : Form
+    {
+        multiplcaciones obj3;
+        restas obj2;
+        divisiones obj4;
+        suma obj1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.checkBox1.Checked == true) {
+                obj1 = new suma();
+                obj1.S1 = double.Parse(val1.Text);
+                obj1.S2 = double.Parse(val2.Text);
+                resultado.Text = obj1.calcular().ToString();
+               
+            }
+            else if (this.checkBox2.Checked == true)
+            {
+                obj2 = new restas();
+                obj2.R1 = double.Parse(val1.Text);
+                obj2.R2 = double.Parse(val2.Text);
+                resultado.Text = obj2.calcular().ToString();
+
+            }
+
+            else if (this.checkBox3.Checked == true)
+            {
+                obj3 = new multiplcaciones();
+                obj3.M1 = double.Parse(val1.Text);
+                obj3.M2 = double.Parse(val2.Text);
+                resultado.Text = obj1.calcular().ToString();
+
+            }
+            else if (this.checkBox4.Checked == true)
+            {
+                obj4 = new divisiones();
+                obj4.D1 = double.Parse(val1.Text);
+                obj4.D2 = double.Parse(val2.Text);
+                resultado.Text = obj4.calcular().ToString();
+
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+        }
+
+        private void checkBox1_Click(object sender, EventArgs e)
+        {
+            
+                checkBox2.Checked = false;
+                checkBox3.Checked = false;
+                checkBox4.Checked = false;
+            
+            
+        }
+
+        private void checkBox2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox2.Checked = false;
+            checkBox1.Checked = false;
+            checkBox4.Checked = false;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Checked = false;
+            checkBox3.Checked = false;
+            checkBox4.Checked = false;
+        }
+
+        private void checkBox4_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+
+        }
+    }
+}
