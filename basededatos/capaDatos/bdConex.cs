96fc5282e68053e7a45f9aadb8e43f879c484019
brayanvaliente2019 <48190483+brayanvaliente2019@users.noreply.github.com>
2019-04-30 19:59:53 -0600

    Add files via upload

---
+++ basededatos/capaDatos/bdConex.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data.Common;
+using System.Data.Sql;
+namespace capaDatos
+{
+    public class bdConex
+    {
+        string cadenaConex = "Data Source=.;Persist Security Info=True;User ID=juan_perez2"
+    
+        public bool conex(){
+        bool correcto=false;
+        try {
+            correcto=true;
+        }
+        catch{Exception }
+    }
+        
+        
+        
+        
+        
+        
+        }
+}
---
+++ basededatos/capaPresentacion/frmVendor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using capaNeg;
+namespace capaPresentacion
+{
+    public partial class frmVendor : Form
+    {
+        manttoVendor mtt = new manttoVendor();
+
+        public frmVendor()
+        {
+            InitializeComponent();
+        }
+
+        private void frmVendor_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
